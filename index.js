d8930dcd2d7029a1eab8d81c72d7c47da3881cb5
rubonics <rubenrodriguez+github@gmail.com>
2019-02-21 15:22:25 -0500

    updates indexing schema
    + updates doc to be a string instead of an object. there is a limit of 1000 different possible shapes for a document type. since everything is stored in the _doc type, we need to standardize the available fields. to do this we stringify the document.
    + adds name key to _source. the name is an important query parameter that all documents will have, so we pull it put into its own field so that we can query it with a weight

--- index.js
+++ index.js
@@ -140,7 +140,8 @@ function WebHookElasticSearch  ( opts ) {
           else {
             needsUpdate = true;
             updateObject = {
-              doc: indexableSiteDataItem,
+              doc: JSON.stringify( indexableSiteDataItem ),
+              name: indexableSiteDataItem.name,
               contentType: indexedItem._source.contentType,
               oneOff: indexedItem._source.oneOff,
             }
@@ -211,11 +212,13 @@ function WebHookElasticSearch  ( opts ) {
         }
         var sourceObject = { contentType: item_type }
         if ( item_type === item_id ) {
-          sourceObject.doc = siteData.data[ item_type ]
+          sourceObject.doc = JSON.stringify( siteData.data[ item_type ] )
           sourceObject.oneOff = true;
+          sourceObject.name = siteData.data[ item_type ].name;
         } else {
-          sourceObject.doc = siteData.data[ item_type ][ item_id ]
+          sourceObject.doc = JSON.stringify( siteData.data[ item_type ][ item_id ] )
           sourceObject.oneOff = false;
+          sourceObject.name = siteData.data[ item_type ][ item_id ].name;
         }
 
         return [ createCommand, sourceObject ];
